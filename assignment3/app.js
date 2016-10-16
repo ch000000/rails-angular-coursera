b65c67ba69610622663638067bd6096496be55a3
Catherine Hart <catherine@the-harts.co.uk>
2016-10-16 13:28:52 +0100

    Assignment 3

--- assignment3/app.js
+++ assignment3/app.js
@@ -2,82 +2,85 @@
 'use strict';
 
    angular.module('NarrowItDownApp', [])
-   .controller('NarrowItDownController', NarrowItDownController)
-   .service('MenuSearchService', MenuSearchService)
-   //.factory('MenuSearchFactory', MenuSearchFactory)
+   .controller('NarrowItDownController', NarrowItDownController) 
+   .factory('MenuSearchFactory', MenuSearchFactory)
    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
-   .directive('foundItems', FoundItemsDirective); 
+   .directive('foundItems', FoundItemsDirective);  
    
-   //NarrowItDownController.$inject = ['MenuSearchFactory']; 
-   NarrowItDownController.$inject = ['MenuSearchService']; 
-   
-      function FoundItemsDirective(){
+   function FoundItemsDirective(){
       var ddo = {
          templateUrl: 'menuList.html',
          scope: {
             items: '<',
-            myTitle: '@title',
             onRemove: '&'
          },
          controller: FoundItemsDirectiveController,
          controllerAs: 'list',
          bindToController: true
-      };
-
+      }; 
       return ddo; 
-   }
+   } 
    
    function FoundItemsDirectiveController() {
       var list = this;  
+      list.emptyList = function() { 
+         if (!list.items) return false;
+         if (list.items.length === 0 ) return true;  
+      }
+   }  
+   
+   NarrowItDownController.$inject = ['MenuSearchFactory']; 
+   function NarrowItDownController(MenuSearchFactory) {
+      var filterCtrl = this;  
+      var menuList = MenuSearchFactory();
        
-   }
-   
-   
-   function NarrowItDownController(MenuSearchService) {
-      var menuList = this; 
-      menuList.title = 'Dishes Found';
-      menuList.filter = function () {
-         var foundItems = [];
-         var promise = MenuSearchService.getMatchedMenuItems();
+      filterCtrl.filter = function () {
+         var found = [];
+         var promise = menuList.getMatchedMenuItems();
          promise.then(function (response) {
             var responseList = response.data; 
               for (var i = 0; i < responseList.menu_items.length; i ++ ) {
-              if (responseList.menu_items[i].name.toLowerCase().search(menuList.searchTerms) > -1 )
-                foundItems.push(responseList.menu_items[i]);
+              if (filterCtrl.searchTerms !== "" && responseList.menu_items[i].description.toLowerCase().indexOf(filterCtrl.searchTerms) > -1 ) 
+                found.push(responseList.menu_items[i]);
             }
-            return foundItems;
+            return found;
          })
          .catch(function (error) {
             console.log("Something went terribly wrong.");
          }); 
-         menuList.foundItems = foundItems;  
-      };
-   }   
+        filterCtrl.items = found;   
+      };  
+     
+      filterCtrl.removeItem = function (itemIndex) { 
+         menuList.removeItem(this.items, itemIndex);  
+      };  
+   }  
    
-
-
-   
-   MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+   MenuSearchFactory.$inject = ['$http', 'ApiBasePath'];
    
    function MenuSearchService($http, ApiBasePath) {
-      var service = this; 
+      var service = this;  
+      
       service.getMatchedMenuItems= function (){
          var response = $http({
             method: "GET",
             url: (ApiBasePath + "/menu_items.json")
-         }) ;
-          return response;  
-      };   
-   } 
-
-   function MenuSearchFactory() {
-     var factory = function () {
-       return new MenuSearchService();
+         });
+         return response;  
+      };  
+      
+      service.removeItem = function (items, itemIndex) {
+         items.splice(itemIndex, 1);
      };
-
-     return factory;
-   }
-
+   }  
+   
+   function MenuSearchFactory($http, ApiBasePath) {
+      var factory = function () {
+         return new MenuSearchService($http, ApiBasePath);
+      }; 
+      return factory;
+   } 
+   
 })();
 
    
